c6161450a6e9fe8e932d9d85e502910479d44eca
Yevgen <107062952+Yevgen1984@users.noreply.github.com>
2022-10-04 15:50:17 +0300

    add comment2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import {UserCard} from './Profile/UserCard';
 import user from './Profile/user.json';
-import {StatisticDate} from './Statistics/data';
+// import {StatisticDate} from './Statistics/data';
 
 export const App = () => {
   return (
     <> 
     
     <UserCard firstUser = {user}/>; 
-    <StatisticDate statisticDate = {data}/>  
+    {/* <StatisticDate statisticDate = {data}/>   */}
 
        </>
   );
